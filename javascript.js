8f7f03066e0cb07212df4881278979b060f85305
Olivia Letendart <letendartolivia@gmail.com>
2019-09-24 08:51:00 +0200

    correction javascript page button and input

--- javascript.js
+++ javascript.js
@@ -1,4 +1,40 @@
-const button = document.querySelector("button");
-const click = document.querySelector("input");
-const ul = document.querySelector("ul");
-const li = document.createElement("li");
+'use strict';
+
+const button = document.body.querySelector("button");
+const input = document.body.querySelector("input");
+const ul = document.body.querySelector("ul");
+let li;
+
+function returnInputLength() {
+    return input.value.length;
+}
+function createElementLi() {
+    li = document.createElement("li");
+}
+function addInputValue() {
+    li.append(document.createTextNode(input.value));
+}
+function insertLiList() {
+    ul.insertAdjacentElement('beforeend', li);
+}
+function cleanInput() {
+    input.value= "";
+}
+
+function keyPressValue(key) {
+    if (key.keyCode === 13) {
+        addItemsList();
+    }
+}
+
+function addItemsList() {
+    if (returnInputLength() > 0) {
+        createElementLi();
+        addInputValue();
+        insertLiList();
+        cleanInput();
+    }
+}
+
+input.addEventListener("keypress", keyPressValue);
+button.addEventListener("click", addItemsList);
